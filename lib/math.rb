c42687c002d76165efb962b435bd43a469734507
Colten Appleby <colten96@gmail.com>
2021-02-14 21:57:56 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,25 @@
 def addition(num1, num2)
+  sum = num1 + num2
+  return sum
 
 end
 
 def subtraction(num1, num2)
-
+  return num2-num1
 end
 
 def division(num1, num2)
-
+  return num2/num1
 end
 
 def multiplication(num1, num2)
-
+  return num1*num2
 end
 
 def modulo(num1, num2)
-
+  return num2 % num1      
 end
 
 def square_root(num)
-
+  return sqrt(num)
 end
